b11f106d6ac0876566a9af15e4900d3bb77ddf32
Toben <zxcv1867@naver.com>
2025-07-24 00:09:34 +0900

    mvp: add button and event handler in effort page

---
+++ lib/heads_up_web/live/effort_live.ex
@@ -0,0 +1,36 @@
+defmodule HeadsUpWeb.EffortLive do
+  use HeadsUpWeb, :live_view
+
+  def mount(_params, _sessions, socket) do
+    {:ok, assign(socket, responders: 0, minutes_per_responder: 10)}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="effort">
+      <section>
+        <button phx-click="add" phx-value-quantity="3">
+          + 3
+        </button>
+        <div>
+          {@responders}
+        </div>
+        &times;
+        <div>
+          {@minutes_per_responder}
+        </div>
+        =
+        <div>
+          {@responders * @minutes_per_responder}
+        </div>
+      </section>
+    </div>
+    """
+  end
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    socket = update(socket, :responders, &(&1 + String.to_integer(quantity)))
+
+    {:noreply, socket}
+  end
+end
