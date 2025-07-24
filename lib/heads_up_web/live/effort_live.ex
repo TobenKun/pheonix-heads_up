bf63707cad42f284548b4887fe38cb924d666294
Sanghyun Shin <zxcv1867@naver.com>
2025-07-24 18:07:03 +0900

    mvp: add tick msg handler

--- lib/heads_up_web/live/effort_live.ex
+++ lib/heads_up_web/live/effort_live.ex
@@ -2,6 +2,10 @@ defmodule HeadsUpWeb.EffortLive do
   use HeadsUpWeb, :live_view
 
   def mount(_params, _sessions, socket) do
+    if connected?(socket) do
+      Process.send_after(self(), :tick, 2000)
+    end
+
     IO.inspect(self(), label: "MOUNT")
     {:ok, assign(socket, responders: 0, minutes_per_responder: 10)}
   end
@@ -48,5 +52,10 @@ defmodule HeadsUpWeb.EffortLive do
     socket = assign(socket, :minutes_per_responder, String.to_integer(minutes))
 
     {:noreply, socket}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 2000)
+    {:noreply, update(socket, :responders, &(&1 + 3))}
   end
 end
